81d0714e8e03e34d2255f59dfb7b768c5a74bff5
Dezainer <gameboymasterz@gmail.com>
2019-11-23 14:34:42 -0300

    AddeAdded Bolsonaro's Pistols xD

--- src/messageGenerator.js
+++ src/messageGenerator.js
@@ -1,24 +1,24 @@
 let messages = [
   obj =>
-    `No mes de ${obj.month} de ${obj.year} nós gastamos R$ ${obj.value} em ${obj.field}. GRANDE DIA \u1f448 \u1f448!`,
+    `No mes de ${obj.month} de ${obj.year} nós gastamos R$ ${obj.value} em ${obj.field}. GRANDE DIA 👈 👈!`,
   obj =>
-    `Em ${obj.month} de ${obj.year}, foi investido pelo governo R$ ${obj.value} destinado à ${obj.field}, GRANDE DIA \u1f448 \u1f448!`,
+    `Em ${obj.month} de ${obj.year}, foi investido pelo governo R$ ${obj.value} destinado à ${obj.field}, GRANDE DIA 👈 👈!`,
   obj =>
-    `Nesse ano de ${obj.year}, foi envestido um total de R$ ${obj.value}, destinado à ${obj.field}. GRANDE DIA \u1f448 \u1f448!`,
+    `Nesse ano de ${obj.year}, foi envestido um total de R$ ${obj.value}, destinado à ${obj.field}. GRANDE DIA 👈 👈!`,
   obj =>
-    `No mês de ${obj.month} de ${obj.year}, nós torramos R$ ${obj.value} em ${obj.field}. GRANDE DIA \u1f448 \u1f448!!`,
+    `No mês de ${obj.month} de ${obj.year}, nós torramos R$ ${obj.value} em ${obj.field}. GRANDE DIA 👈 👈!!`,
   obj =>
-    `No mês de ${obj.month}, nos orgulhamos de gastar R$ ${obj.value} em ${obj.field}, GRANDE DIA \u1f448 \u1f448!`,
+    `No mês de ${obj.month}, nos orgulhamos de gastar R$ ${obj.value} em ${obj.field}, GRANDE DIA 👈 👈!`,
   obj =>
-    `Em ${obj.month} de ${obj.year}, para ${obj.field} gastamos R$ ${obj.value}. GRANDE DIA \u1f448 \u1f448!`,
+    `Em ${obj.month} de ${obj.year}, para ${obj.field} gastamos R$ ${obj.value}. GRANDE DIA 👈 👈!`,
   obj =>
-    `No mês de ${obj.month} de ${obj.year}, gastamos R$ ${obj.value} em ${obj.field}, GRANDE DIA \u1f448 \u1f448!`,
+    `No mês de ${obj.month} de ${obj.year}, gastamos R$ ${obj.value} em ${obj.field}, GRANDE DIA 👈 👈!`,
   obj =>
-    `Em ${obj.year} no mês de ${obj.month}, investimos R$ ${obj.value} em ${obj.field}, obrigado capitalistas opressores, GRANDE DIA \u1f448 \u1f448!.`,
+    `Em ${obj.year} no mês de ${obj.month}, investimos R$ ${obj.value} em ${obj.field}, obrigado capitalistas opressores, GRANDE DIA 👈 👈!.`,
   obj =>
-    `No mês de ${obj.month} de ${obj.year}, cuidamos muito bem do seu R$ ${obj.value} em um ${obj.field}, GRANDE DIA \u1f448 \u1f448!`,
+    `No mês de ${obj.month} de ${obj.year}, cuidamos muito bem do seu R$ ${obj.value} em um ${obj.field}, GRANDE DIA 👈 👈!`,
   obj =>
-    `No mês de ${obj.month} de ${obj.year} jogamos fora R$ ${obj.value} em ${obj.field} GRANDE DIA \u1f448 \u1f448!`
+    `No mês de ${obj.month} de ${obj.year} jogamos fora R$ ${obj.value} em ${obj.field} GRANDE DIA 👈 👈!`
 ];
 
 export default obj => {
